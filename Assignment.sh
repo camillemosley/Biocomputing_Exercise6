03bf33b74f58bd8b183b109cd32530db1316842c
Noah Barkoff <nbarkoff@nd.edu>
2023-10-06 08:52:44 -0400

    Final Exercise 6

--- Assignment.sh
+++ Assignment.sh
@@ -1,6 +1,6 @@
 
 
-cat wages.csv | tail -n +2 | cut -d , -f 1,2 | sed 's/,/ /' | sort -t ' ' -k 1,1 -k 2n > UniqueWages.txt
+cat wages.csv | tail -n +2 | cut -d , -f 1,2 | sed 's/,/ /' | sort -u -t ' ' -k 1,1 -k 2n > UniqueWages.txt
 
 highest_earner=$(cat wages.csv | tail -n +2 | cut -d , -f 1,2,4 | sort -t , -k 3,3 -n | tail -n 1)
 
@@ -8,10 +8,10 @@ lowest_earner=$(cat wages.csv | tail -n +2 | cut -d , -f 1,2,4 | sort -t , -k 3,
 
 females_top10=$(cat wages.csv | tail -n +2 | cut -d , -f 1,2,4 | sort -t , -k 3,3 -n | tail -n 10 | grep -c -E "female")
 
-min_12=$(cat wages.csv | tail -n +2 | cut -d , -f 1,2,4 | grep ",12," | sort -t , -k 3,3 -n | head -n 1 | cut -d , -f 3)
-min_16=$(cat wages.csv | tail -n +2 | cut -d , -f 1,2,4 | grep ",16," | sort -t , -k 3,3 -n | head -n 1 | cut -d , -f 3)
+min_12=$(cat wages.csv | tail -n +2 | cut -d , -f 3,4 | grep -E "12," | sort -t , -k 2,2 -n | head -n 1 | cut -d , -f 2)
+min_16=$(cat wages.csv | tail -n +2 | cut -d , -f 3,4 | grep -E "16," | sort -t , -k 2,2 -n | head -n 1 | cut -d , -f 2)
 
-diff=$(echo "$min_12 - $min_16" | bc)
+diff=$(echo "$min_16 - $min_12" | bc)
 
 echo "1) Highest earner: $highest_earner"
 echo "2) Lowest earner: $lowest_earner"
